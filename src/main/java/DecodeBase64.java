71076b23e30a37c01934c898f0fe9f10b446dca4
ats3md <ats3md@umsystem.edu>
2022-07-09 14:49:34 -0500

    Added logic to decode Base64 string.

--- src/main/java/DecodeBase64.java
+++ src/main/java/DecodeBase64.java
@@ -1,8 +1,22 @@
 import org.apache.commons.codec.binary.Base64;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class DecodeBase64 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String encodedString = "SSBsb3ZlIG15IENNUCBTQ0kgMjI2MSBjbGFzcyBzbyBtdWNoLCBJIHdpc2ggSSBjb3VsZCBiZSBqdXN0IGFzIGNvb2wgYXMgbXkgcHJvZmVzc29y";
+        String userInput;
+
+        String decodedString = new String(Base64.decodeBase64(encodedString), StandardCharsets.UTF_8);
+
+        System.out.println("Encodeded String: " + encodedString);
+        System.out.println("Decoded to: " + decodedString);
+
+
+
 
     }
 }
